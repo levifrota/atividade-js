89f344219a5a730ad1c827744d733ce396985ee9
Levi Frota <71519800+levifrota@users.noreply.github.com>
2021-11-02 12:52:24 -0300

    novos arquivos

--- index.js
+++ index.js
@@ -1,74 +1,64 @@
-class Item {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
+var comandas = JSON.parse(localStorage.getItem("comandas")) || [
+  { nome: "Comanda Vazia", preco: 0 },
+];
+
+var elLista = document.getElementById("lista");
+var elProduto = document.getElementById("produto");
+var elBotao1 = document.getElementById("botao1");
+var elPrice = document.getElementById("preco");
+
+elBotao1.onclick = function () {
+  var nome = elProduto.value;
+  var preco = elPrice.value;
+
+  comandas.push({ nome: nome });
+  comandas.push({ preco: preco });
+  elProduto.value = "";
+  elPrice.value = "";
+  salvarComanda();
+  listarComanda();
+};
+
+function salvarComanda() {
+  localStorage.setItem("comandas", JSON.stringify(comandas));
 }
 
-class Bill {
-  constructor() {
-    this.items = [];
-    this.total = 0;
+function listarComanda() {
+  elLista.innerHTML = "";
+  for (const comanda of comandas) {
+    var elPedido = document.createElement("dt");
+    var elListaPreco = document.createElement("dd");
+    var elNome = document.createTextNode(comanda.nome);
+    var elPreco = document.createTextNode(comanda.preco);
+
+    var elExcluir = document.createElement("a");
+    elExcluir.setAttribute("href", "#");
+    elExcluir.onclick = function () {
+      comandas = comandas.filter(function (item) {
+        return item.nome !== comanda.nome;
+      });
+      comandas = comandas.filter(function (item) {
+        return item.preco !== comanda.preco;
+      });
+      salvarComanda();
+      listarComanda();
+    };
+
+    var elExcluirTexto = document.createTextNode("Excluir");
+
+    elExcluir.appendChild(elExcluirTexto);
+    elLista.appendChild(elPedido);
+    elListaPreco.appendChild(elPreco);
+    elPedido.appendChild(elNome);
+    elPedido.appendChild(elExcluir);
   }
-
-  addItem = (item) => {
-    this.items.push(item);
-    this.render();
-    this.billTotal();
-  };
-
-  removeItem = (nome) => {
-    //TODO
-  };
-
-  billTotal = () => {
-    /*var total = this.items.reduce((acumulador,valorAtual)=>{
-        return acumulador + valorAtual.price},0);
-        console.log(total);*/
-
-      var total = 0;
-      for(var i=0; i < this.items.length; i++ ){
-        total += this.items[i].price;
-      }
-
-      console.log(total);
-      document.getElementById('total').innerHTML = 'R$ ' + total;
-    
-  };
-
-  render = () => {
-    //UNFINISHED
-    let billContainer = document.getElementById("items");
-    billContainer.innerHTML = "";
-    this.items.map((item) => {
-      let row = document.createElement("tr");
-      let foodName = document.createElement("td");
-      let foodPrice = document.createElement("td");
-      foodName.innerHTML = item.name;
-      foodPrice.innerHTML = "R$ " + item.price;
-
-      row.append(foodName);
-      row.append(foodPrice);
-      billContainer.append(row);
-    });
-  };
 }
-
-var bill = new Bill();
+listarComanda();
 
 function init() {
-  bill.addItem(new Item("croissant", 5.99));
-  bill.addItem(new Item("café", 2.99));
-  bill.render();
   document.getElementsByTagName("body")[0].style.display = "flex";
 }
 
 function printBill() {
   window.print();
 }
-
-function adicionarItem(){
-  bill.addItem(new Item('agua', 1));
-}
-
-//document ou query selector (inputs)
